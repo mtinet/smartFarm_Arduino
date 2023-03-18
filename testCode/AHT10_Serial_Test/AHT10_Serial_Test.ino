8b4c4700c31930f4eda60778097e7a5d120cfc30
Jason Juhyun Kim <mtinet@hanmail.net>
2023-03-18 20:04:57 +0900

    Add files via upload

---
+++ testCode/AHT10_Serial_Test/AHT10_Serial_Test.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <AHT10.h>
+
+AHT10Class AHT10;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin();
+  if(AHT10.begin(eAHT10Address_Low))
+    Serial.println("Init AHT10 Success.");
+  else
+    Serial.println("Init AHT10 Failure.");
+}
+
+void loop() {
+  Serial.println(String("")+"Humidity(%RH):\t\t"+AHT10.GetHumidity()+"%");
+  Serial.println(String("")+"Temperature('C):\t"+AHT10.GetTemperature()+"'C");
+  Serial.println(String("")+"Dewpoint('C):\t\t"+AHT10.GetDewPoint()+"'C");
+  delay(500);
+}
---
+++ testCode/LiquidCrystal_I2C_test/LiquidCrystal_I2C_test.ino
@@ -0,0 +1,37 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // LCD I2C 주소: 0x27
+
+void setup() {
+  // LCD 초기 설정, 오프닝 이벤트    
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Hello,SmartFarm!");
+  lcd.setCursor(0,1);
+  lcd.print("Made by M2Hands!");
+}
+
+void loop() {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
